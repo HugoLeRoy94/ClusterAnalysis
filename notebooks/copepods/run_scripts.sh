4cb6a7208d6b748c19ef6e0aae5ea2c796b26281
HugoLeRoy94 <leroyhugo@yahoo.fr>
2025-07-24 12:17:55 +0200

    run on the copepods

--- notebooks/copepods/run_scripts.sh
+++ notebooks/copepods/run_scripts.sh
@@ -14,21 +14,25 @@
         --output-file ../../data/copepods/interim/entropy_scan.csv \
         --columns-trans x,y,z \
         --K-values 3,5,10,20,30,40,50 \
-        --n-clusters-values 2,3,4,5,10,15,20,30,40,50,100,200 \
-        --tau 1
+        --n-clusters-values 2,3,4,5,10,15,20,30,40,50,100,250,500,750,1000,2000,3000,4000,5000 \
+        --tau 1 \
+        --n-trajectories 1000 \
+        --n-windows 5000
+
 
 ../../scripts/03_embed_and_cluster.py \
-        --input data/copepods/interim/phases.parquet \
-        --output-dir data/copepods/processed \
-        --columns speed,curvature_angle,torsion_angle \
-        --K 30 \
-        --n-clusters 30 \
-        --tau 1,2,5,10 \
-        --groupby label
+        --input ../../data/copepods/interim/phases.parquet \
+        --output-dir ../../data/copepods/processed \
+        --columns-trans x,y,z \
+        --K 50 \
+        --n-clusters 2000 \
+        --tau 1,2,3,5,7,10,25,50,100,200 \
+        --groupby label \
+        --n-trajectories 100
 
 ../../scripts/04_find_umap.py \
-        --embedding data/copepods/processed/embedding.pkl \
-        --output data/copepods/processed/umap.npy \
+        --input ../../data/copepods/processed/ \
+        --output ../..data/copepods/processed/umap.npy \
         --subsample 1000 \
         --cluster-centers True \
         --n-neighbors 100 \
